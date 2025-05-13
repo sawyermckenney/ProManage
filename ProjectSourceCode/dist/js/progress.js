bcd74baf7cd95ad1343c68c97d78e4048c275c42
Sawyer McKenney <sawyermckenney@gmail.com>
2025-05-13 10:52:45 -0600

    Clean slate for gh-pages

--- ProjectSourceCode/dist/js/progress.js
+++
@@ -1,8 +0,0 @@
-// document.addEventListener(
-//   "DOMContentLoaded",
-//   loadProgress()
-// );
-const progressBar = document.getElementById("progress-bar");
-progress = Math.floor(100 * document.currentScript.getAttribute("progress"));
-progressBar.style = `width: ${progress}%`;
-progressBar.innerHTML = `${progress}%`;
--- ProjectSourceCode/public/js/manager_search.js
+++
@@ -1,8 +0,0 @@
-function updateResults(event, data) {
-  const managerField = document.getElementById("manager");
-  let currentText = managerField.value;
-  data.sort((a, b) => {
-    a - b;
-  });
-  console.log(data);
-}
